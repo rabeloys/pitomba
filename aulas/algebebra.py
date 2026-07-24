242a7c4b046a2a3cf9204497786f331a535aba12
diogo <92460672+rabeloys@users.noreply.github.com>
2026-07-24 12:01:21 -0300

    Create algebebra.py

---
+++ aulas/algebebra.py
@@ -0,0 +1,14 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+r = n1 % n2
+print('Soma: {}'.format(s))
+print('Produto: {}'.format(m))
+print('Potêncialização: {}'.format(e))
+print('Divisão: {:.2f}'.format(d))
+print('Divisão Inteira: {}'.format(di))
+print('Resto da Divisão: {}'.format(r))
