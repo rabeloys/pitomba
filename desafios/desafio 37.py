27bf68c650cb324eb9cb8948e3d40513c06731cd
diogo <92460672+rabeloys@users.noreply.github.com>
2026-07-27 15:21:21 -0300

    Update desafio 37.py

---
+++ desafios/desafio 37.py
@@ -0,0 +1,2 @@
+# Não aprendi sobre como mexer na conversão para binario, hexadecimal e mais algum outro que eu esqueci o nome, então por enquanto vai ficar assim até eu ter coragem de aprender isso.
+# Quando eu aprender eu resolvo (pretendo)
