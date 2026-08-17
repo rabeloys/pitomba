22f4161d959a5505609e67aa99baaf2d89a09268
diogo <92460672+rabeloys@users.noreply.github.com>
2026-08-17 20:53:24 -0300

    Replace counting loop with user input for even/odd

--- aulas/while.py
+++ aulas/while.py
@@ -1,4 +1,14 @@
+pares = 0
+impares = 0
 n = 1
-while n <= 10:
-    print(n)
-    n += 1
+numeros = []
+while n != 0 :
+    n = int(input('Digite um numero: '))
+    numeros.append(n)
+    if n % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+print(f'\nTotal de números digitados: {len(numeros)}')
+print(f'Total de pares: {pares}')
+print(f'Total de ímpares: {impares}')
